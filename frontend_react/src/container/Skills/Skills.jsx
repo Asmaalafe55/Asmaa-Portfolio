b29b888c4afa6aa12736b19138336927bc5625d0
Asmaalafe55 <asmaalafe55@gmail.com>
2022-11-12 17:14:24 +0200

    adding skills, fetching from sanity, removing some pictures

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,13 +1,52 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-
 import './Skills.scss';
+
 const Skills = () => {
-  return <div>Skills</div>;
+  const [skills, setSkills] = useState([]);
+  const [experiences, setExperiences] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "skills"]';
+    const exQuery = '*[_type == "experiences"]';
+    client.fetch(query).then((data) => {
+      setSkills(data);
+    });
+    client.fetch(exQuery).then((data) => {
+      setExperiences(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skils-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skils-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex "
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
 };
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
