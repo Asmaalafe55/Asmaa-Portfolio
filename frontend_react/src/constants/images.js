07b03914e52933683042e69b80f48ed1e575e4b9
Asmaalafe55 <asmaalafe55@gmail.com>
2022-11-13 18:41:52 +0200

    fix some eslint issues

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -9,6 +9,7 @@ import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   email,
   mobile,
